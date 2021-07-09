1ffcfa2fda310f28ae7e29ad540385fcd36237b1
Adhetya Narayan <adhetya.narayan@gmail.com>
2021-07-09 10:30:26 +0530

    Added Ridobiko assignment

---
+++ Ridobiko/app.js
@@ -0,0 +1,12 @@
+document.getElementById("button").style.display = "none";
+
+function toggleDisplay() {
+    var dropdown = document.getElementById("settings");
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    }
+    else {
+        dropdown.style.display = "none";
+    }
+}
+
