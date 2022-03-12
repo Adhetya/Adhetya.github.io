0eabc3619d54c58f69b41b0ace68541190cff07d
Adhetya Narayan <adhetya.narayan@gmail.com>
2022-03-12 12:16:54 +0530

    Redesigned contact form

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ toggleButton.addEventListener("click", () => {
 document.querySelector(".theme-toggle-button").addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
+
+function focusFunction() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+
+}
+function blurFunction() {
+    let parent = this.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+
+}
+
+const inputs = document.querySelectorAll(".input");
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunction);
+    input.addEventListener("blur", blurFunction);
+})
