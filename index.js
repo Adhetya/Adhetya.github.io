3c9c973c7c28c0f63de58d7378cd55df3cc6e019
Adhetya Narayan <adhetya.narayan@gmail.com>
2022-03-20 14:17:28 +0530

    Redesign navigation bar

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-});
+// toggleButton.addEventListener("click", () => {
+//     navbarLinks.classList.toggle("active");
+// });
 
 document.querySelector(".theme-toggle-button").addEventListener("click", () => {
     document.body.classList.toggle("dark");
